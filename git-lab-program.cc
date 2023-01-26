29df6526feedf4388d07a436399cb215b85d5c5d
Nick Taylor <nstaylor2001@gmail.com>
2023-01-25 20:53:58 -0500

    moving along

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nick Taylor>
+ *        Date: <January 25th 2023>
+ * Description: Copied code I stole from the lab page
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
